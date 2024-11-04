20975c63cad8f1d314a09611454d8eb21cb6b42f
stock169 <58556976+stock169@users.noreply.github.com>
2024-11-04 21:32:22 +0800

    Update c-diy-part1.sh

--- c-diy-part1.sh
+++ c-diy-part1.sh
@@ -6,5 +6,5 @@ rm packages/network/bluez/package.mk
 wget -P packages/network/bluez/ https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/packages/network/bluez/package.mk
 rm packages/compress/zlib/package.mk
 wget -P packages/compress/zlib/ https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/packages/compress/zlib/package.mk
-rm projects/Amlogic-ce/devices/Amlogic-no/packages/linux/package.mk
-wget -P projects/Amlogic-ce/devices/Amlogic-no/packages/linux/ https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/projects/Amlogic-ce/devices/Amlogic-no/packages/linux/package.mk
+#rm projects/Amlogic-ce/devices/Amlogic-no/packages/linux/package.mk
+#wget -P projects/Amlogic-ce/devices/Amlogic-no/packages/linux/ https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/projects/Amlogic-ce/devices/Amlogic-no/packages/linux/package.mk
